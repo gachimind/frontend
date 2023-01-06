8b6f11606d108ea6ba069ec43fcf44a9033b961d
heayounchoi <heayounchoi@gmail.com>
2023-01-06 17:37:22 +0900

    CHORE: redux store 초기 설정

---
+++ src/redux/store.ts
@@ -0,0 +1,15 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+const rootReducer = combineReducers({});
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+  devTools: process.env.NODE_ENV !== 'production',
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
