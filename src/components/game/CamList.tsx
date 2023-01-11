db2de1fdd1edd3fdb02051d8f893beb328b4dfe6
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-01-12 00:54:04 +0900

    FIX: 불필요 코드 삭제

--- src/components/game/CamList.tsx
+++ src/components/game/CamList.tsx
@@ -1,19 +1,6 @@
-import { useAppSelector } from '@redux/hooks';
-
 // TODO: 사용자들의 캠 대신 이름으로 참여 여부를 먼저 나타냈다. 수정되어야 한다.
 const CamList = () => {
-  const { room } = useAppSelector((state) => state.gameRoom);
-  const { user } = useAppSelector((state) => state.user);
-
-  return (
-    <div>
-      {room?.participants.map((participant, index) => (
-        <p key={participant.userId}>
-          {index === 0 && '(방장)'} {participant.nickname} {participant.userId === user?.userId && '(나)'}
-        </p>
-      ))}
-    </div>
-  );
+  return <div></div>;
 };
 
 export default CamList;
