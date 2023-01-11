0277784e59b4df9f181ac9d2f88c2333dcfc8909
heayounchoi <heayounchoi@gmail.com>
2023-01-11 23:32:54 +0900

    FEAT: 메인페이지 유저정보 섹션 로그인 버튼 추가

--- src/components/home/UserInfo.tsx
+++ src/components/home/UserInfo.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 
 import CreateGameModal from './CreateGameModal';
+import LoginModal from './LoginModal';
 
 const UserInfo = () => {
+  const [loginModalVisible, setLoginGameModalVisible] = useState<boolean>(false);
   const [createGameModalVisible, setCreateGameModalVisible] = useState<boolean>(false);
+
   return (
     <div>
+      {loginModalVisible && <LoginModal visible={loginModalVisible} onClose={() => setLoginGameModalVisible(false)} />}
+      <button onClick={() => setLoginGameModalVisible(true)}>로그인</button>
       {createGameModalVisible && (
         <CreateGameModal visible={createGameModalVisible} onClose={() => setCreateGameModalVisible(false)} />
       )}
