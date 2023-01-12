34546a8a8d472a26e110f2e64463fa91fb426cc4
heayounchoi <heayounchoi@gmail.com>
2023-01-12 17:52:40 +0900

    FEAT: 폰트 globalstyle 적용

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,35 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import { SingletonHooksContainer } from 'react-singleton-hook';
+import { createGlobalStyle } from 'styled-components';
 
 import Main from '@pages/Main';
 import Mypage from '@pages/Mypage';
 import Room from '@pages/Room';
 import store from '@redux/store';
 
+// TODO: globalstyle 다시 적용하기
 const App = (): JSX.Element => {
   return (
-    <Provider store={store}>
-      <SingletonHooksContainer />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/room/:id" element={<Room />} />
-          <Route path="/mypage" element={<Mypage />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <>
+      <GlobalStyle />
+      <Provider store={store}>
+        <SingletonHooksContainer />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/room/:id" element={<Room />} />
+            <Route path="/mypage" element={<Mypage />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </>
   );
 };
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300&display=swap');
+  font-family: 'IBM Plex Mono', monospace;
+`;
 
 export default App;
