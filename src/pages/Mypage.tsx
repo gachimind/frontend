a8122714b44072a1eaf3cf2bb397f37d18a803d4
heayounchoi <heayounchoi@gmail.com>
2023-01-15 17:38:20 +0900

    FEAT: 마이페이지 UI 추가

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,5 +1,17 @@
+import UserInfo from '@components/home/UserInfo';
+import ContentBox from '@components/layout/ContentBox';
+import MyPageLayout from '@components/layout/mypage/MyPageLayout';
+
 const Mypage = () => {
-  return <div></div>;
+  return (
+    <MyPageLayout>
+      <ContentBox title="SCORE">
+        <UserInfo />
+      </ContentBox>
+      <ContentBox title="GROWTH TOWER"></ContentBox>
+      <ContentBox title="KEYWORD LIST"></ContentBox>
+    </MyPageLayout>
+  );
 };
 
 export default Mypage;
