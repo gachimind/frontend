b32468af3a741037ce0b4233f204bea9e6493a81
heayounchoi <heayounchoi@gmail.com>
2023-01-15 17:29:50 +0900

    FEAT: theme, globalstyle 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,30 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import { SingletonHooksContainer } from 'react-singleton-hook';
+import { ThemeProvider } from 'styled-components';
 
 import Main from '@pages/Main';
 import Mypage from '@pages/Mypage';
 import Room from '@pages/Room';
 import store from '@redux/store';
+import { GlobalStyle } from '@styles/GlobalStyle';
+import { theme } from '@styles/theme';
 
 const App = (): JSX.Element => {
   return (
-    <Provider store={store}>
-      <SingletonHooksContainer />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/room/:id" element={<Room />} />
-          <Route path="/mypage" element={<Mypage />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Provider store={store}>
+        <SingletonHooksContainer />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/room/:id" element={<Room />} />
+            <Route path="/mypage" element={<Mypage />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </ThemeProvider>
   );
 };
 
