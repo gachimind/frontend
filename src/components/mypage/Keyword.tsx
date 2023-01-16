4773937264bc61e9cef2311b9870a7af35c0a4e3
heayounchoi <heayounchoi@gmail.com>
2023-01-16 15:03:44 +0900

    FEAT: 키워드 컴포넌트 추가

---
+++ src/components/mypage/Keyword.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const Keyword = () => {
+  return (
+    <KeywordLayout>
+      <KeywordBox></KeywordBox>
+      <KeywordBox></KeywordBox>
+    </KeywordLayout>
+  );
+};
+
+export default Keyword;
+
+const KeywordLayout = styled.div`
+  height: 94.3%;
+  padding: 40px;
+  gap: 36px;
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+`;
+
+const KeywordBox = styled.div`
+  background-color: white;
+`;
