5bfb1945d039e21867a62d6a7199520906b16ce6
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-01-16 17:01:25 +0900

    FEAT: 캠 목록 유저 상태 표시 컴포넌트 추가

---
+++ src/components/game/CamUserStatus.tsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+interface CamUserStatusProps {
+  nickname: string;
+  isCamOn?: boolean;
+  isMicOn?: boolean;
+}
+
+const CamUserStatus = ({ nickname, isCamOn, isMicOn }: CamUserStatusProps) => {
+  return (
+    <CamUserStatusLayout>
+      <div>
+        {/* TODO: 아이콘 넣기 */}
+        <NicknameText>{nickname}</NicknameText>
+      </div>
+      <div>{/* TODO: 아이콘 넣기 */}</div>
+    </CamUserStatusLayout>
+  );
+};
+const CamUserStatusLayout = styled.div`
+  position: absolute;
+  bottom: 10px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 8px;
+  align-items: center;
+  width: 150px;
+  height: 35px;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const NicknameText = styled.span`
+  font-family: ${(props) => props.theme.font.korean};
+  font-weight: 700;
+  font-size: 12px;
+  color: white;
+`;
+
+export default CamUserStatus;
