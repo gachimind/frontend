6b021728153759c19f2c52caf6f2a6a636f089a2
heayounchoi <heayounchoi@gmail.com>
2023-01-17 14:02:53 +0900

    FIX: 스코어보드 임시 이미지 삭제

--- src/components/game/ScoreBoard.tsx
+++ src/components/game/ScoreBoard.tsx
@@ -1,15 +1,5 @@
-import styled from 'styled-components';
-
-// TODO: 임시 이미지로 추후 수정되어야 한다.
-import ScoreBoardImage from '@assets/svg_scoreBoardImage.svg';
-
 const ScoreBoard = () => {
-  return <BoardImage src={ScoreBoardImage} />;
+  return <div></div>;
 };
-
-const BoardImage = styled.img`
-  width: 408px;
-  height: 624px;
-`;
 
 export default ScoreBoard;
