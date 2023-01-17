217d760d6a73e82172c543d91c60904992eef7fa
heayounchoi <heayounchoi@gmail.com>
2023-01-17 13:38:22 +0900

    FIX: 사이즈 조정 및 컴포넌트명 변경

--- src/components/layout/RoomTemplate.tsx
+++ src/components/layout/RoomTemplate.tsx
@@ -17,51 +17,51 @@ const RoomTemplate = ({ children }: { children: React.ReactNode }) => {
       <Header page="ROOM"></Header>
       <RoomContentsBox>{children}</RoomContentsBox>
       <Footer>
-        <FooterButtonsBox>
+        <FooterBox>
           <RuleButton src={GameRuleIcon} />
           <MediaControlBox>
             <CamButton />
             <MicButton />
           </MediaControlBox>
-          <LeaveRoomButton>나가기</LeaveRoomButton>
-        </FooterButtonsBox>
+          <LeaveButton>나가기</LeaveButton>
+        </FooterBox>
       </Footer>
     </PageContainer>
   );
 };
 
 const RoomContentsBox = styled.div`
-  height: 684px;
+  background-color: green;
+  height: 664px;
   gap: 40px;
   padding: 0px 152px;
   display: grid;
   grid-template-columns: 3fr 4fr 3fr;
 `;
 
-const FooterButtonsBox = styled.div`
+const FooterBox = styled.div`
   height: inherit;
-  gap: 50px;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
 const RuleButton = styled.img`
-  margin-right: 550px;
+  margin-right: 613px;
 `;
 
 const MediaControlBox = styled.div`
-  gap: 53px;
+  gap: 55px;
   display: flex;
 `;
 
-const LeaveRoomButton = styled.button`
+const LeaveButton = styled.button`
   font-family: ${(props) => props.theme.font.korean};
   font-size: 20px;
   background-color: ${(props) => props.theme.colors.darkGrey1};
   width: 200px;
   height: 48px;
-  margin-left: 450px;
+  margin-left: 454px;
 `;
 
 export default RoomTemplate;
