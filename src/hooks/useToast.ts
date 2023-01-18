ff9ff87a603d5ec0b0941964e173322b050188cd
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-01-19 05:02:20 +0900

    FEAT: useToast hook 추가

---
+++ src/hooks/useToast.ts
@@ -0,0 +1,32 @@
+import { useRef } from 'react';
+
+import { Id, toast } from 'react-toastify';
+
+const IconType = {
+  success: '😽',
+  info: '🐱',
+  error: '😿',
+};
+
+const useToast = () => {
+  const toastRef = useRef<Id>();
+
+  const notify = (message: string, type: 'info' | 'success' | 'error') => {
+    dismissToast();
+    toastRef.current = toast[type](message + IconType[type]);
+  };
+
+  function checkToastExists() {
+    return toastRef.current && toast.isActive(toastRef.current);
+  }
+
+  function dismissToast() {
+    if (checkToastExists()) {
+      toastRef.current && toast.dismiss(toastRef.current);
+    }
+  }
+
+  return { notify };
+};
+
+export default useToast;
