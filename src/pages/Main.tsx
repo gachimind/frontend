af2b05ff0b64bb027fd9481e981f8ea889ee9b34
heayounchoi <heayounchoi@gmail.com>
2023-01-19 18:45:46 +0900

    FIX: 임시 토큰 설정 삭제

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,4 @@
-import { useEffect } from 'react';
-
 import { useAuthSocket } from '@hooks/socket/useAuthSocket';
-import { useAppDispatch } from '@redux/hooks';
-import { __getUserInfo } from '@redux/modules/userSlice';
 
 import RoomList from '@components/home/RoomList';
 import UserInfo from '@components/home/UserInfo';
@@ -10,13 +6,6 @@ import ContentContainer from '@components/layout/ContentContainer';
 import MainTemplate from '@components/layout/MainTemplate';
 
 const Main = () => {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    sessionStorage.setItem('accessToken', 'token1');
-    dispatch(__getUserInfo());
-  }, []);
-
   useAuthSocket();
   return (
     <MainTemplate>
