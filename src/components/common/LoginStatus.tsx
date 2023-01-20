2fec4a5c5910048f549fd7fb3a344ae7f81a5e98
heayounchoi <heayounchoi@gmail.com>
2023-01-20 23:30:44 +0900

    FIX: 로직 변경으로 불필요해진 코드 삭제

--- src/components/common/LoginStatus.tsx
+++ src/components/common/LoginStatus.tsx
@@ -2,14 +2,11 @@ import { useEffect } from 'react';
 
 import { useAppDispatch } from '@redux/hooks';
 import { __getUserInfo } from '@redux/modules/userSlice';
-import { getCookie } from '@utils/getCookie';
 
 const LoginStatus = () => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    getCookie('jwt') && sessionStorage.setItem('accessToken', getCookie('jwt').split(' ')[1]);
-
     dispatch(__getUserInfo());
   }, []);
   return <></>;
