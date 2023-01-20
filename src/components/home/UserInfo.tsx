a9c9609ae6c415a20b072edd0131d56db5b12b3a
heayounchoi <heayounchoi@gmail.com>
2023-01-20 20:22:38 +0900

    FIX: 메인페이지 디자인 반영

--- src/components/home/UserInfo.tsx
+++ src/components/home/UserInfo.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 import styled from 'styled-components';
 
+import medalIcon from '@assets/svg_medalIcon.svg';
+import trophyIcon from '@assets/svg_trophyIcon.svg';
 import { useAppSelector } from '@redux/hooks';
 
 import EditProfileModal from '@components/mypage/EditProfileModal';
@@ -22,7 +24,7 @@ const UserInfo = ({ mypage }: { mypage?: boolean }) => {
         <UserImageBox></UserImageBox>
         <UserStatusBox>
           {!isLogined ? (
-            <span>로그인이 필요한 서비스입니다.</span>
+            <span>로그인이 필요합니다.</span>
           ) : (
             <>
               <span className="nickname">닉네임</span>
@@ -48,35 +50,49 @@ const UserInfo = ({ mypage }: { mypage?: boolean }) => {
       )}
       {mypage && <MakeRoomButton onClick={() => setEditProfileModalVisible(true)}>회원정보 수정</MakeRoomButton>}
       <ScoreBox>
-        <img></img>
+        <img src={medalIcon} />
         <div>
           <span className="title">오늘 획득한 점수</span>
-          <span className="score">10000</span>
+          <span className="score">
+            10000
+            <span className="title" id="fix">
+              점
+            </span>
+          </span>
         </div>
       </ScoreBox>
       <ScoreBox>
-        <img></img>
+        <img src={trophyIcon} />
         <div>
           <span className="title">누적 점수</span>
-          <span className="score">10000</span>
+          <span className="score">
+            10000
+            <span className="title" id="fix">
+              점
+            </span>
+          </span>
         </div>
       </ScoreBox>
     </UserInfoLayout>
   );
 };
 
-// TODO: 임시 색상으로 추후 변경되어야 한다.
 const UserInfoLayout = styled.div`
   height: 94.3%;
-  padding: 40px;
-  gap: 16px;
+  padding: 32px 40px;
+  gap: 12px;
   display: grid;
   grid-template-rows: 4fr 1fr 1fr 1fr;
 `;
 
 const ProfileBox = styled.div`
-  display: grid;
-  grid-template-rows: 3fr 1fr;
+  position: relative;
+  margin-bottom: 18px;
+
+  border-top: ${(props) => props.theme.borders.normalblack};
+  border-right: ${(props) => props.theme.borders.normalwhite};
+  border-bottom: ${(props) => props.theme.borders.normalwhite};
+  border-left: ${(props) => props.theme.borders.normalblack};
 `;
 
 const UserImageBox = styled.div`
@@ -84,9 +100,14 @@ const UserImageBox = styled.div`
 `;
 
 const UserStatusBox = styled.div`
+  position: absolute;
   font-size: 20px;
-  background-color: ${(props) => props.theme.colors.darkGrey1};
-  gap: 53px;
+  color: ${(props) => props.theme.colors.ivory2};
+  background: rgba(0, 0, 0, 0.7);
+  width: 100%;
+  bottom: 0;
+  gap: 52px;
+  padding: 25px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -102,10 +123,18 @@ const MakeRoomButton = styled.button`
   cursor: pointer;
   font-family: ${(props) => props.theme.font.korean};
   font-size: 24px;
+  color: ${(props) => props.theme.colors.ivory2};
+  text-shadow: ${(props) => props.theme.textShadow.textShadow};
+  background-color: ${(props) => props.theme.colors.darkGrey2};
+
+  border-top: ${(props) => props.theme.borders.normalwhite};
+  border-right: ${(props) => props.theme.borders.normalblack};
+  border-bottom: ${(props) => props.theme.borders.normalblack};
+  border-left: ${(props) => props.theme.borders.normalwhite};
 `;
 
 const ScoreBox = styled.div`
-  background-color: white;
+  color: ${(props) => props.theme.colors.ivory2};
   font-family: ${(props) => props.theme.font.korean};
   padding: 0px 40px;
   gap: 24px;
@@ -113,6 +142,11 @@ const ScoreBox = styled.div`
   grid-template-columns: 1fr 4fr;
   justify-content: center;
   align-items: center;
+
+  border-top: ${(props) => props.theme.borders.normalblack};
+  border-right: ${(props) => props.theme.borders.normalwhite};
+  border-bottom: ${(props) => props.theme.borders.normalwhite};
+  border-left: ${(props) => props.theme.borders.normalblack};
 
   img {
     background-color: ${(props) => props.theme.colors.darkGrey1};
@@ -125,11 +159,20 @@ const ScoreBox = styled.div`
     flex-direction: column;
 
     .title {
+      font-family: ${(props) => props.theme.font.korean};
       font-size: 12px;
     }
 
     .score {
+      font-family: ${(props) => props.theme.font.korean};
+      text-shadow: ${(props) => props.theme.textShadow.textShadow};
       font-size: 24px;
+      gap: 4px;
+      display: flex;
+      align-items: center;
+      #fix {
+        text-shadow: none;
+      }
     }
   }
 `;
