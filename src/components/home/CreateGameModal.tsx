8cf423f6de269d2b2e4b873b5fa21562344ef927
heayounchoi <heayounchoi@gmail.com>
2023-01-24 17:01:11 +0900

    FIX: 비밀방 버튼 icon html element로 변경, 비밀번호 인풋 활성/비활성화 기능 추가

--- src/components/home/CreateGameModal.tsx
+++ src/components/home/CreateGameModal.tsx
@@ -17,6 +17,7 @@ import { CreateRoomRequest } from '@customTypes/socketType';
 
 // TODO: 모든 input을 추가하고 유효성 검사를 수행하여 방을 생성할 수 있어야 한다.
 const CreateGameModal = ({ visible, onClose }: { visible: boolean; onClose: () => void }) => {
+  const [showPasswordInput, setShowPasswordInput] = useState<boolean>(false);
   const [roomTitle, setRoomTitle] = useState<string>('');
   const [maxCount, setMaxCount] = useState<number>(2);
   const [time, setTime] = useState<string>('30:30:60');
@@ -47,17 +48,23 @@ const CreateGameModal = ({ visible, onClose }: { visible: boolean; onClose: () =
     <Modal visible={visible} onClose={onClose} title="MAKE A ROOM">
       <CreateGameModalLayout>
         <InputContainer label="방제">
-          <Input
-            style={{ width: '340px' }}
-            type="text"
-            value={roomTitle}
-            onChange={(e) => setRoomTitle(e.target.value)}
-          />
-          <img style={{ position: 'absolute', right: '68px', marginTop: '34px' }} src={lockIcon} />
+          <TitleInputBox>
+            <Input
+              style={{ width: '328px' }}
+              type="text"
+              value={roomTitle}
+              onChange={(e) => setRoomTitle(e.target.value)}
+            />
+            <PasswordButton onClick={() => setShowPasswordInput((prev) => !prev)}>
+              <img src={lockIcon} />
+            </PasswordButton>
+          </TitleInputBox>
         </InputContainer>
-        <InputContainer label="비밀번호">
-          <Input type="text" value={roomPassword} onChange={(e) => setRoomPassword(e.target.value)} />
-        </InputContainer>
+        {showPasswordInput && (
+          <InputContainer label="비밀번호">
+            <Input type="text" value={roomPassword} onChange={(e) => setRoomPassword(e.target.value)} />
+          </InputContainer>
+        )}
         <InputContainer label="인원">
           <Selection options={PARTICIPANTS_OPTIONS} setValue={setMaxCount} />
         </InputContainer>
@@ -75,6 +82,18 @@ const CreateGameModalLayout = styled.div`
   gap: 24px;
   display: flex;
   flex-direction: column;
+`;
+
+const TitleInputBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const PasswordButton = styled(Button)`
+  width: 56px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const CreateRoomButton = styled(Button)`
