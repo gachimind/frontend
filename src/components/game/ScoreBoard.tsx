79bc5dffaea08cc12728f1bfb2c13e9739d45d65
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-01-26 07:35:23 +0900

    FEAT: 스코어보드에 임시로 스코어 반영 확인할 수 있는 텍스트

--- src/components/game/ScoreBoard.tsx
+++ src/components/game/ScoreBoard.tsx
@@ -1,5 +1,23 @@
+import styled from 'styled-components';
+
+import { useAppSelector } from '@redux/hooks';
+
+// TODO: 임시로 점수만 나타낸 것을 수정해야함
 const ScoreBoard = () => {
-  return <div></div>;
+  const { room } = useAppSelector((state) => state.gameRoom);
+  return (
+    <ScoreBoardLayout>
+      {room?.participants.map((participant) => (
+        <p key={participant.userId}>
+          {participant.nickname} : {participant.score ?? 0}
+        </p>
+      ))}
+    </ScoreBoardLayout>
+  );
 };
+
+const ScoreBoardLayout = styled.div`
+  color: ${(props) => props.theme.colors.white};
+`;
 
 export default ScoreBoard;
