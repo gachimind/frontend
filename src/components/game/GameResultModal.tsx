15832c1ee85d2fee255a750e63b8035ce5811769
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-01-26 03:12:58 +0900

    FEAT: 게임 종료 시 결과 모달 창 띄우기

--- src/components/game/GameResultModal.tsx
+++ src/components/game/GameResultModal.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+
 import styled from 'styled-components';
 
 import Button from '@components/common/Button';
@@ -5,14 +7,15 @@ import Modal from '@components/common/Modal';
 
 // TODO: SCORE 폰트가 만약 한번만 사용되면 구글 링크가 없는 관계로 이미지를 활용한다.
 const GameResultModal = ({ visible, onClose }: { visible: boolean; onClose: () => void }) => {
+  const navigate = useNavigate();
   return (
     <Modal visible={visible} onClose={onClose} title="SCORE" width={640}>
       <GameResultModalLayout>
         <span>SCORE</span>
         <ResultBox>점수냥이</ResultBox>
         <ButtonBox>
-          <LeaveRoomButton>나가기</LeaveRoomButton>
-          <ContinueGameButton>계속하기</ContinueGameButton>
+          <LeaveRoomButton onClick={() => navigate('/', { replace: true })}>나가기</LeaveRoomButton>
+          <ContinueGameButton onClick={onClose}>계속하기</ContinueGameButton>
         </ButtonBox>
       </GameResultModalLayout>
     </Modal>
--- src/components/game/Presenter.tsx
+++ src/components/game/Presenter.tsx
@@ -1,8 +1,11 @@
+import { useEffect, useState } from 'react';
+
 import styled, { keyframes } from 'styled-components';
 
 import { useAppSelector } from '@redux/hooks';
 
 import GameReady from './GameReady';
+import GameResultModal from './GameResultModal';
 import GameStart from './GameStart';
 import PresentationInfo from './PresentationInfo';
 import PresenterCam from './PresenterCam';
@@ -12,10 +15,17 @@ const Presenter = () => {
   const { user } = useAppSelector((state) => state.user);
   const { room } = useAppSelector((state) => state.gameRoom);
   const { turn, playState } = useAppSelector((state) => state.gamePlay);
+  const [resultModalVisible, setResultModalVisible] = useState<boolean>(false);
   const currentUser = room?.participants.find((participant) => participant.userId === user?.userId);
   const presenterNickname =
     room?.participants.find((participant) => participant.userId === turn?.speechPlayer)?.nickname ?? '';
   const isMe = user?.userId === turn?.speechPlayer;
+
+  useEffect(() => {
+    if (playState?.event === 'gameEnd') {
+      setResultModalVisible(true);
+    }
+  }, [playState]);
 
   return (
     <PresenterLayout>
@@ -38,6 +48,9 @@ const Presenter = () => {
           {currentUser?.isHost && <GameStart />}
           {currentUser?.isHost === false && <GameReady />}
         </GameReadyBox>
+      )}
+      {resultModalVisible && (
+        <GameResultModal visible={resultModalVisible} onClose={() => setResultModalVisible(false)} />
       )}
     </PresenterLayout>
   );
