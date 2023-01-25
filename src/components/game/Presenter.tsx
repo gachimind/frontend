ec4fb2eedf9662795af970f4b3f1d058f078e406
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-01-25 16:07:35 +0900

    FEAT: 발표자 캠에 현재 설명할 키워드 정보 식별되도록 추가

--- src/components/game/Presenter.tsx
+++ src/components/game/Presenter.tsx
@@ -1,9 +1,6 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-import MicOnIcon from '@assets/svg_micOnIcon.svg';
 import { useAppSelector } from '@redux/hooks';
-
-import PlayerImageHolder from '@components/game/PlayerImageHolder';
 
 import GameReady from './GameReady';
 import GameStart from './GameStart';
@@ -18,23 +15,19 @@ const Presenter = () => {
   const currentUser = room?.participants.find((participant) => participant.userId === user?.userId);
   const presenterNickname =
     room?.participants.find((participant) => participant.userId === turn?.speechPlayer)?.nickname ?? '';
+  const isMe = user?.userId === turn?.speechPlayer;
 
   return (
     <PresenterLayout>
       {turn && currentUser && playState?.event === 'readyTimer' && (
-        <PresentationInfo
-          isMe={user?.userId === turn.speechPlayer}
-          keyword={turn.keyword}
-          nickname={presenterNickname}
-        />
+        <PresentationInfo isMe={isMe} keyword={turn.keyword} nickname={presenterNickname} />
       )}
-      {room?.isGameOn && turn && (
-        <PresenterCam
-          nickname={presenterNickname}
-          isMe={user?.userId === turn.speechPlayer}
-          userId={turn.speechPlayer}
-        />
+      {isMe && playState?.event === 'speechTimer' && (
+        <PresenterKeywordBox>
+          제시어:&nbsp;<span>{turn?.keyword}</span>
+        </PresenterKeywordBox>
       )}
+      {room?.isGameOn && turn && <PresenterCam nickname={presenterNickname} isMe={isMe} userId={turn.speechPlayer} />}
       {!room?.isGameOn && <GameReadyBox>{currentUser?.isHost ? <GameStart /> : <GameReady />}</GameReadyBox>}
     </PresenterLayout>
   );
@@ -47,11 +40,43 @@ const PresenterLayout = styled.div`
   height: 100%;
 `;
 
-// TODO: 디자인 보고 수정
 const GameReadyBox = styled.div`
   position: absolute;
   top: 110px;
   right: -40px;
+`;
+
+const KeywordSlide = keyframes`
+0% {
+  height: 220px;
+  background-color: inherit;
+  font-size: 40px;
+  color: red;
+}
+  100% {
+    height: 42px;
+    background-color: rgba(28, 28, 28, 0.7);
+  }
+`;
+
+const PresenterKeywordBox = styled.div`
+  position: absolute;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: flex-end;
+  top: 38px;
+  background-color: rgba(28, 28, 28, 0.7);
+  z-index: 3;
+  padding: 10px 0;
+  color: ${(props) => props.theme.colors.white};
+  height: 42px;
+  animation: ${KeywordSlide} 0.75s 0s;
+  font-size: 14px;
+  & > span {
+    font-size: 20px;
+    font-weight: 500;
+  }
 `;
 
 export default Presenter;
