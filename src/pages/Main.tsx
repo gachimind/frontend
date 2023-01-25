7c5f6ea7378f5618c0bc555b2d30a7969ae57689
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-01-26 07:06:11 +0900

    STYLE: 메인 페이지 불필요 import 제거

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 import { PUBLISH } from '@constants/socket';
 import useErrorSocket from '@hooks/socket/useErrorSocket';
