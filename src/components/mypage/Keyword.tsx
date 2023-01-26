4b146bd6c7bc82432c24d4356b1cd594f7dd0c4b
heayounchoi <heayounchoi@gmail.com>
2023-01-26 13:24:52 +0900

    FEAT: 키워드 박스 디자인 반영

--- src/components/mypage/Keyword.tsx
+++ src/components/mypage/Keyword.tsx
@@ -3,8 +3,12 @@ import styled from 'styled-components';
 const Keyword = () => {
   return (
     <KeywordLayout>
-      <KeywordBox></KeywordBox>
-      <KeywordBox></KeywordBox>
+      <KeywordBox>
+        <div className="keyword-box-header">SOLVED KEYWORDS</div>
+      </KeywordBox>
+      <KeywordBox>
+        <div className="keyword-box-header">PUBLISHED KEYWORDS</div>
+      </KeywordBox>
     </KeywordLayout>
   );
 };
@@ -13,12 +17,22 @@ export default Keyword;
 
 const KeywordLayout = styled.div`
   height: 94.3%;
-  padding: 52px 40px;
-  gap: 36px;
+  padding: 45px 40px;
+  gap: 32px;
   display: grid;
   grid-template-rows: 1fr 1fr;
 `;
 
 const KeywordBox = styled.div`
-  background-color: white;
+  ${(props) => props.theme.borders.bottomRightWhiteBorder}
+
+  .keyword-box-header {
+    font-size: 16px;
+    color: ${(props) => props.theme.colors.ivory2};
+    background-color: rgba(0, 0, 0, 0.7);
+    height: 44px;
+    padding-left: 24px;
+    display: flex;
+    align-items: center;
+  }
 `;
