708c6499cb531181b7feb5526057eac792a1b5d7
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-01-27 05:19:43 +0900

    CHORE: storybook에 router, redux decorator 및 viewport 설정 추가

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -5,21 +5,54 @@ import { theme } from '../src/styles/theme';
 import { GlobalStyle } from '../src/styles/GlobalStyle';
 import '../src/app.css';
 import 'react-tooltip/dist/react-tooltip.css';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../src/redux/store';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  viewport: {
+    viewports: {
+      pc: {
+        name: 'Desktop',
+        styles: {
+          width: '1440px',
+          height: '100%',
+        },
+      },
+    },
+    defaultViewport: 'pc',
+  },
   layout: 'fullscreen',
+};
+
+export const reactRouterDecorator: DecoratorFn = (Story) => {
+  return (
+    <MemoryRouter>
+      <Routes>
+        <Route path="/*" element={<Story />} />
+      </Routes>
+    </MemoryRouter>
+  );
+};
+
+export const reduxDecorator: DecoratorFn = (Story) => {
+  return (
+    <Provider store={store}>
+      <Story />
+    </Provider>
+  );
 };
 
 const withTheme: DecoratorFn = (StoryFn, context) => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <StoryFn id="root" />
+      <StoryFn />
       <div id="modal-root"></div>
       <div id="loading-root"></div>
     </ThemeProvider>
   );
 };
 
-export const decorators = [withTheme];
+export const decorators = [withTheme, reactRouterDecorator, reduxDecorator];
