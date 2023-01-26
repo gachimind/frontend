c9cf42bd3ea99f38a66a1c593608cd73f711672a
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-01-27 05:47:11 +0900

    TEST: Modal 스토리북 추가

---
+++ src/stories/common/modal.stories.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { theme } from '@styles/theme';
+
+import Modal, { ModalProps } from '@components/common/Modal';
+
+export default {
+  title: 'common/Modal',
+  component: Modal,
+  parameters: {
+    controls: {
+      exclude: ['visible', 'onClose'],
+    },
+  },
+} as ComponentMeta<typeof Modal>;
+
+const Template: ComponentStory<typeof Modal> = (args: ModalProps) => {
+  const [visible, setVisible] = useState<boolean>(false);
+  return (
+    <>
+      <button style={{ fontSize: '16px' }} onClick={() => setVisible(true)}>
+        click to show
+      </button>
+      <Modal {...args} visible={visible} onClose={() => setVisible(false)}>
+        <div style={{ height: '400px', color: theme.colors.white }}>hello</div>
+      </Modal>
+    </>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'title',
+};
