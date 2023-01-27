df0e284d5a2448955478bd009e6688bad8163508
heayounchoi <heayounchoi@gmail.com>
2023-01-27 17:02:36 +0900

    FEAT: 유저 키워드 조회 타입 추가

--- src/types/userType.ts
+++ src/types/userType.ts
@@ -12,5 +12,14 @@ export interface MyProfileResponse {
   };
 }
 
+export interface MyKeywordsResponse {
+  userId: number;
+  todayQuizKeyword: Array<string>;
+  todaySpeechKeyword: Array<string>;
+  totalQuizKeyword: Array<string>;
+  totalSpeechKeyword: Array<string>;
+}
+
 // TODO: 현재 로그인한 회원 상태 관리를 위한 타입으로 부가적인 속성들이 필요하다면 추후에 확장하여 사용할 수 있다.
 export type MyProfile = MyProfileResponse;
+export type MyKeywords = MyKeywordsResponse;
