0aa2fde8be58294e8a815209b46cf07431d79edb
heayounchoi <heayounchoi@gmail.com>
2023-01-30 13:27:13 +0900

    FIX: footer 완성 디자인 반영

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,11 +1,14 @@
 import styled from 'styled-components';
 
+import footerImage from '@assets/svg_footerImage.svg';
+
 const Footer = ({ children }: { children?: React.ReactNode }) => {
   return <FooterLayout>{children}</FooterLayout>;
 };
 
 const FooterLayout = styled.div`
-  background-color: ${(props) => props.theme.colors.lightGrey1};
+  background-image: url(${footerImage});
+  background-size: contain;
   height: 80px;
 `;
 
