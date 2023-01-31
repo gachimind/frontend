2e0b109a7fccc19206c386900924902800e2f462
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-01-31 09:58:18 +0900

    FEAT: 게임 페이지의 ScoreBoard UI/기능 추가

--- src/components/game/ScoreBoard.tsx
+++ src/components/game/ScoreBoard.tsx
@@ -1,23 +1,49 @@
 import styled from 'styled-components';
 
+import { BACKGROUND_HEIGHT, BACKGROUND_WIDTH } from '@constants/characters';
 import { useAppSelector } from '@redux/hooks';
 
-// TODO: 임시로 점수만 나타낸 것을 수정해야함
+import StarlightBackground from '@components/common/StarlightBackground';
+
+import PlayerCat from './PlayerCat';
+
 const ScoreBoard = () => {
   const { room } = useAppSelector((state) => state.gameRoom);
+
+  if (!room) {
+    return <ScoreBoardLayout></ScoreBoardLayout>;
+  }
   return (
     <ScoreBoardLayout>
-      {room?.participants.map((participant) => (
-        <p key={participant.userId}>
-          {participant.nickname} : {participant.score ?? 0}
-        </p>
-      ))}
+      <StarlightBackground />
+      <PlayerCatBox>
+        {room.participants.map((participant) => (
+          <PlayerCat key={participant.socketId} participant={participant} />
+        ))}
+      </PlayerCatBox>
     </ScoreBoardLayout>
   );
 };
 
 const ScoreBoardLayout = styled.div`
   color: ${(props) => props.theme.colors.white};
+  position: relative;
+  overflow: hidden;
+  width: ${BACKGROUND_WIDTH + 'px'};
+  height: ${BACKGROUND_HEIGHT + 'px'};
+`;
+
+const PlayerCatBox = styled.div`
+  position: absolute;
+  width: 100%;
+  left: -28px;
+  bottom: -21px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: flex-end;
+  & > * {
+    margin-right: -56px;
+  }
 `;
 
 export default ScoreBoard;
