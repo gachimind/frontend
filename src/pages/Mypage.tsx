680b6583efa8a3999c92d18bc4dd6b8a36c9ae7c
heayounchoi <heayounchoi@gmail.com>
2023-02-01 02:02:55 +0900

    FEAT: 캣타워 추가

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -3,6 +3,7 @@ import { useLayoutEffect } from 'react';
 import UserInfo from '@components/home/UserInfo';
 import ContentContainer from '@components/layout/ContentContainer';
 import MyPageTemplate from '@components/layout/MyPageTemplate';
+import GrowthTower from '@components/mypage/GrowthTower';
 import Keyword from '@components/mypage/Keyword';
 
 const Mypage = () => {
@@ -20,7 +21,9 @@ const Mypage = () => {
       <ContentContainer title="SCORE">
         <UserInfo mypage={true} />
       </ContentContainer>
-      <ContentContainer title="GROWTH TOWER"></ContentContainer>
+      <ContentContainer title="GROWTH TOWER">
+        <GrowthTower />
+      </ContentContainer>
       <ContentContainer title="KEYWORD LIST">
         <Keyword />
       </ContentContainer>
