326aceb13c9292b830ac4f2536ec1681db362b9e
heayounchoi <heayounchoi@gmail.com>
2023-02-03 05:39:33 +0900

    FEAT: 인스타그램 연결 버튼 추가

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,15 +1,42 @@
 import styled from 'styled-components';
 
 import footerImage from '@assets/svg_footerImage.svg';
+import instagramIcon from '@assets/svg_instagramIcon.svg';
 
-const Footer = ({ children }: { children?: React.ReactNode }) => {
-  return <FooterLayout>{children}</FooterLayout>;
+const Footer = ({ children, page }: { children?: React.ReactNode; page?: string }) => {
+  return (
+    <FooterLayout>
+      {page !== 'room' && (
+        <button
+          className="instagram"
+          onClick={() => window.open('https://instagram.com/gachimind?igshid=ZDdkNTZiNTM=')}
+        >
+          <img src={instagramIcon} />
+        </button>
+      )}
+      {children}
+    </FooterLayout>
+  );
 };
 
 const FooterLayout = styled.div`
+  position: relative;
   background-image: url(${footerImage});
   background-size: contain;
   height: 80px;
+
+  .instagram {
+    position: absolute;
+    background-color: transparent;
+    right: 60px;
+    margin-top: 13px;
+    img {
+      background: linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%);
+      border-radius: 20px;
+      width: 56px;
+      height: 56px;
+    }
+  }
 `;
 
 export default Footer;
