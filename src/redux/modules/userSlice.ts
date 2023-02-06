621d5bd3d3218b91398539ce517c7840d1abecd9
heayounchoi <heayounchoi@gmail.com>
2023-02-06 20:13:22 +0900

    STYLE: 불필요한 redux thunk 코드 삭제

--- src/redux/modules/userSlice.ts
+++ src/redux/modules/userSlice.ts
@@ -1,5 +1,4 @@
-import userApi from '@apis/userApi';
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 import { MyKeywords, MyProfile } from '@customTypes/userType';
 
@@ -15,36 +14,6 @@ const initialState: InitialUserStateType = {
   keywords: null,
 };
 
-export const __getUserInfo = createAsyncThunk('getUserInfo', async (_, thunkAPI) => {
-  try {
-    const userInfo = await userApi.getUserInfo();
-    return thunkAPI.fulfillWithValue(userInfo);
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error);
-  }
-});
-
-export const __getUserKeyword = createAsyncThunk('getUserKeyword', async (_, thunkAPI) => {
-  try {
-    const userKeywords = await userApi.getUserKeyword();
-    return thunkAPI.fulfillWithValue(userKeywords);
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error);
-  }
-});
-
-export const __updateUserInfo = createAsyncThunk(
-  'updateUserInfo',
-  async ({ newNickname, newProfileImg }: { newNickname: string; newProfileImg: string }, thunkAPI) => {
-    try {
-      await userApi.updateUserInfo({ newNickname, newProfileImg });
-      return thunkAPI.fulfillWithValue({ newNickname, newProfileImg });
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error);
-    }
-  },
-);
-
 const userSlice = createSlice({
   name: 'user',
   initialState,
@@ -54,25 +23,6 @@ const userSlice = createSlice({
       state.user = null;
       sessionStorage.clear();
     },
-  },
-  extraReducers: (builder) => {
-    builder.addCase(__getUserInfo.fulfilled, (state, action) => {
-      state.isLogined = true;
-      state.user = action.payload;
-    });
-    builder.addCase(__getUserInfo.rejected, (state) => {
-      state.isLogined = false;
-    });
-    builder.addCase(__getUserKeyword.fulfilled, (state, action) => {
-      state.keywords = action.payload;
-    });
-    builder.addCase(__updateUserInfo.fulfilled, (state, action) => {
-      if (state.user) {
-        state.user.profileImg = action.payload.newProfileImg;
-        state.user.nickname = action.payload.newNickname;
-        return;
-      }
-    });
   },
 });
 
