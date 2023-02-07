0a7dd7d047ab6b2cd7199c53ba2fcc7158565ff7
heayounchoi <heayounchoi@gmail.com>
2023-02-08 04:50:56 +0900

    STYLE: 쿼리 사용으로 불필요해진 코드 삭제

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 const BASE_URL = process.env.REACT_APP_API_ENDPOINT;
 
 const noAuthInstance = axios.create({ baseURL: BASE_URL });
-const authInstance = axios.create({ baseURL: BASE_URL });
 
 noAuthInstance.interceptors.response.use(
   (response) => {
@@ -18,29 +17,4 @@ noAuthInstance.interceptors.response.use(
   },
 );
 
-authInstance.interceptors.request.use((config) => {
-  config.headers = {
-    authorization: 'Bearer ' + sessionStorage.getItem('accessToken'),
-  };
-
-  return config;
-});
-
-authInstance.interceptors.response.use(
-  (response) => {
-    return response.data.data;
-  },
-  (error) => {
-    if (error.response?.status === 401) {
-      sessionStorage.clear();
-      window.location.assign('/');
-    }
-    const errorResponse = {
-      ...error.response.data,
-      status: error.response.status,
-    };
-    return Promise.reject(errorResponse);
-  },
-);
-
-export { authInstance, noAuthInstance };
+export { noAuthInstance };
--- src/apis/userApi.ts
+++ src/apis/userApi.ts
@@ -1,12 +1,10 @@
-import { authInstance, noAuthInstance } from '@apis/instance';
+import { noAuthInstance } from '@apis/instance';
 
 const USER_API = '/api/users';
 
 const userApi = {
   // 닉네임 중복 검사
   duplicateCheck: (newNickname: string) => noAuthInstance.get(USER_API + `/${newNickname}`),
-  // 로그아웃
-  logout: () => authInstance.get(USER_API + '/logout'),
 };
 
 export default userApi;
